b13b6e9f1dec0a996c5273c8165445a3b3a3bbb8
Andy Bonventre <andybons@meetneptune.com>
2026-01-20 14:16:56 -0500

    feat(trace): implement netlink socket setup and subscription

--- internal/trace/tracer_linux.go
+++ internal/trace/tracer_linux.go
@@ -28,12 +28,6 @@ const (
 	_NETLINK_CONNECTOR = 11
 )
 
-// Blank identifiers to satisfy compiler until imports are used
-var (
-	_ = binary.LittleEndian
-	_ = syscall.AF_NETLINK
-)
-
 // ProcConnectorTracer implements process tracing using Linux proc connector.
 // Requires CAP_NET_ADMIN or root privileges.
 type ProcConnectorTracer struct {
@@ -62,10 +56,65 @@ func NewProcConnectorTracer(cfg Config) (*ProcConnectorTracer, error) {
 }
 
 func (t *ProcConnectorTracer) Start() error {
-	return fmt.Errorf("not implemented")
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if t.started {
+		return fmt.Errorf("tracer already started")
+	}
+
+	// Create netlink socket
+	sock, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_DGRAM, _NETLINK_CONNECTOR)
+	if err != nil {
+		return fmt.Errorf("create netlink socket: %w (requires CAP_NET_ADMIN or root)", err)
+	}
+	t.sock = sock
+
+	// Bind to process events group
+	addr := &syscall.SockaddrNetlink{
+		Family: syscall.AF_NETLINK,
+		Groups: _CN_IDX_PROC,
+		Pid:    uint32(syscall.Getpid()),
+	}
+	if err := syscall.Bind(sock, addr); err != nil {
+		syscall.Close(sock)
+		return fmt.Errorf("bind netlink socket: %w", err)
+	}
+
+	// Subscribe to process events
+	if err := t.subscribe(true); err != nil {
+		syscall.Close(sock)
+		return fmt.Errorf("subscribe to process events: %w", err)
+	}
+
+	// Initialize tracked PIDs
+	if t.config.PID > 0 {
+		t.trackedPIDs[t.config.PID] = true
+	}
+
+	t.started = true
+	t.wg.Add(1)
+	go t.readLoop()
+
+	return nil
 }
 
 func (t *ProcConnectorTracer) Stop() error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if !t.started {
+		return nil
+	}
+
+	close(t.done)
+	_ = t.subscribe(false)
+	syscall.Close(t.sock)
+
+	t.wg.Wait()
+	close(t.events)
+	t.started = false
+
 	return nil
 }
 
@@ -77,6 +126,50 @@ func (t *ProcConnectorTracer) OnExec(cb func(ExecEvent)) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	t.callbacks = append(t.callbacks, cb)
+}
+
+// subscribe sends a message to subscribe/unsubscribe from process events.
+func (t *ProcConnectorTracer) subscribe(listen bool) error {
+	op := uint32(_PROC_CN_MCAST_IGNORE)
+	if listen {
+		op = uint32(_PROC_CN_MCAST_LISTEN)
+	}
+
+	// Build message: nlmsghdr + cn_msg + op
+	// Total size: 16 (nlhdr) + 20 (cnhdr) + 4 (op) = 40 bytes
+	buf := make([]byte, 40)
+
+	// Netlink header
+	binary.LittleEndian.PutUint32(buf[0:], 40)                        // len
+	binary.LittleEndian.PutUint16(buf[4:], syscall.NLMSG_DONE)        // type
+	binary.LittleEndian.PutUint16(buf[6:], 0)                         // flags
+	binary.LittleEndian.PutUint32(buf[8:], 1)                         // seq
+	binary.LittleEndian.PutUint32(buf[12:], uint32(syscall.Getpid())) // pid
+
+	// Connector header
+	binary.LittleEndian.PutUint32(buf[16:], _CN_IDX_PROC) // id.idx
+	binary.LittleEndian.PutUint32(buf[20:], _CN_VAL_PROC) // id.val
+	binary.LittleEndian.PutUint32(buf[24:], 1)            // seq
+	binary.LittleEndian.PutUint32(buf[28:], 0)            // ack
+	binary.LittleEndian.PutUint16(buf[32:], 4)            // len (op size)
+	binary.LittleEndian.PutUint16(buf[34:], 0)            // flags
+
+	// Operation
+	binary.LittleEndian.PutUint32(buf[36:], op)
+
+	// Send to kernel (pid=0)
+	addr := &syscall.SockaddrNetlink{
+		Family: syscall.AF_NETLINK,
+		Groups: _CN_IDX_PROC,
+		Pid:    0,
+	}
+	return syscall.Sendto(t.sock, buf, 0, addr)
+}
+
+func (t *ProcConnectorTracer) readLoop() {
+	defer t.wg.Done()
+	// Will be implemented in next task
+	<-t.done
 }
 
 // Compile-time interface check
